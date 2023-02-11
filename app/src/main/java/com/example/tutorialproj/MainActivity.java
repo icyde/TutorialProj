13499b46f61d4300aa3870c6051e10298191198d
icyde <brandontanruien@gmail.com>
2023-02-11 18:27:10 +0800

    some methods

--- app/src/main/java/com/example/tutorialproj/MainActivity.java
+++ app/src/main/java/com/example/tutorialproj/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +17,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void disable( View v) {
+        /*
         v.setEnabled(false);
         Button b = (Button) v;
         b.setText("disabled");
         //Log.d("success", "Button disabled.");
+         */
+
+        /*
+        View myView = findViewById(R.id.button);
+        myView.setEnabled(false);
+        Button button3 = (Button) myView;
+        button3.setText("New Disabled");
+         */
+        findViewById(R.id.button).setEnabled(false);
+        ((Button) findViewById(R.id.button2)).setText("new new disabled");
+    }
+    public void handleText(View v) {
+        TextView t = findViewById(R.id.source);
+        String input = t.getText().toString();
+        Log.d("info", input);
     }
 }

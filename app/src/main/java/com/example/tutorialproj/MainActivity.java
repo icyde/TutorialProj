10d6f7b13ef9af07f7fc0fd69a69523a444b0ec0
icyde <brandontanruien@gmail.com>
2023-02-11 23:00:45 +0800

    Launching another activity

--- app/src/main/java/com/example/tutorialproj/MainActivity.java
+++ app/src/main/java/com/example/tutorialproj/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.tutorialproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,28 +17,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Home"); //set label for main activity
 
     }
-    public void disable( View v) {
-        /*
+   /* public void disable( View v) {
+
+        //set button to disable
+
         v.setEnabled(false);
         Button b = (Button) v;
         b.setText("disabled");
-        //Log.d("success", "Button disabled.");
+        //Log.d("success", "Button disabled."); // show up on logcat
          */
 
         /*
+        //point to a view
+
         View myView = findViewById(R.id.button);
         myView.setEnabled(false);
+        //downcast to button to change button text
         Button button3 = (Button) myView;
         button3.setText("New Disabled");
-         */
+
+         //alternative methods
         findViewById(R.id.button).setEnabled(false);
-        ((Button) findViewById(R.id.button2)).setText("new new disabled");
+        ((Button) findViewById(R.id.button2)).setText("new new disabled"); //cast view to button
     }
+
+
     public void handleText(View v) {
-        TextView t = findViewById(R.id.source);
+        EditText t = findViewById(R.id.source);
         String input = t.getText().toString();
-        Log.d("info", input);
+        //get text and set as output
+        ((TextView)findViewById(R.id.output)).setText(input);
+        Toast.makeText(this, input, Toast.LENGTH_LONG).show(); //alert that will fade away
+    }
+
+         */
+    public void launchSettings(View v) {
+        // launch a new activity
+        Intent i = new Intent(this, SettingsActivity.class ); //(from this instance, to target instance)
+        i.putExtra("COOL", "Hello"); //(key-value pair) -> access value using key
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/tutorialproj/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.example.tutorialproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Intent i = getIntent();
+        String message = i.getStringExtra("COOL");
+        ((TextView) findViewById(R.id.textview2)).setText(message);
+    }
+}
